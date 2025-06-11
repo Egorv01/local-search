e55b5fc415f950ef8b804a054840d0dc74901aec
mcantillon21 <mcan@stanford.edu>
2025-06-11 11:23:14 -0700

    Initial Commit

---
+++ WWDC-MLX/WWDC_MLXApp.swift
@@ -0,0 +1,17 @@
+//
+//  WWDC_MLXApp.swift
+//  WWDC-MLX
+//
+//  Created by MollyCantillon on 6/11/25.
+//
+
+import SwiftUI
+
+@main
+struct WWDC_MLXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
